237e4442743d245a690cca2c2775befdc20d12ae
ardi5432 <130774240+ardi5432@users.noreply.github.com>
2025-10-03 13:43:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,5 @@ def run_hybrid_recommendation(id_user, preferences, alpha=0.9):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
